f6095098d6a739ba41e0732eb93638fbacbff3e3
Fabien Weyh <fabien.weyh@free.fr>
2016-11-06 15:42:22 +0100

    Configure serveur and store

--- src/server.js
+++ src/server.js
@@ -2,4 +2,16 @@ import Server from 'socket.io';
 
 export default function startServer() {
     const io = new Server().attach(8090)
+
+    store.subscribe(
+        () => io.emit('state', store.getState().toJS())
+        // We are sending the whole state each time, maybe this could be improved
+    );
+
+    io.on('connection', (socket) => {
+        socket.emit('state', store.getState().toJS());
+        socket.on('action', store.dispatch.bind(store));
+        // TODO: Add authentification here
+    });
+
 }
